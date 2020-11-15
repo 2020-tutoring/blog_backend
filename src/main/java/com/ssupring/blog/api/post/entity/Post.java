acd08bd63ed5199d0101a14b0caf619e599fd57c
LeeJongKeun <johnny550@naver.com>
2020-11-15 15:26:39 +0900

    no message

--- src/main/java/com/ssupring/blog/api/post/entity/Post.java
+++ src/main/java/com/ssupring/blog/api/post/entity/Post.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import lombok.Builder;
 import lombok.Getter;
 
 @Entity
@@ -12,7 +14,23 @@ public class Post {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  Long postId;
 
+  private Long postId;
+
+  private String author;
+
+  private String title;
+
+  private String body;
+
+
+
+  @Builder
+  public Post(Long id, String author, String title, String body) {
+    this.postId = id;
+    this.author = author;
+    this.title = title;
+    this.body = body;
+  }
 
 }
--- src/main/java/com/ssupring/blog/api/post/service/PostService.java
+++ src/main/java/com/ssupring/blog/api/post/service/PostService.java
@@ -1,8 +1,11 @@
 package com.ssupring.blog.api.post.service;
 
+import com.ssupring.blog.api.post.entity.Post;
 import com.ssupring.blog.api.post.entity.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +13,29 @@ public class PostService {
 
   private final PostRepository postRepository;
 
+  public PostService(PostRepository postRepository) {
+    this.postRepository = postRepository;
+  }
+
+/*
+  public List<PosTto> getPostList() {
+    List<Post> postList = PostRepository.findAll();
+    List<PosTto> posttoList = new ArrayList<>();
+
+    for(Post post : postList) {
+      PosTto postto = PosTto.builder()
+              .id(post.getId())
+              .author(post.getAuthor())
+              .title(post.getTitle())
+              .content(post.getContent())
+              .createdDate(post.getCreatedDate())
+              .build();
+      posttoList.add(postDto);
+    }
+    return posttoList;
+  }
+*/
+  public static void deletePost(Long postId){
+    PostRepository.deleteById(postId);
+  }
 }
